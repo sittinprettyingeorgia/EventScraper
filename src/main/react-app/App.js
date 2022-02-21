1c08ada4e9d02e88e4ef8f3a930697a16e963100
sittinprettyingeorgia <mitchellblake29@aol.com>
2022-02-21 06:42:06 -0800

    scraping is successful adding react

--- src/main/react-app/App.js
+++ src/main/react-app/App.js
@@ -1,31 +1,62 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { LIST } from './constants/pageConstants';
 
 function App() {
   const [data, setData] = useState();
+  const [query, setQuery] = useState();
 
   useEffect(() => {
-    const getData = async() => {
-
+    const postData = async() => {
       let pages = {
-        typeOfData: "event",
-        pages: LIST
-      }
+        "typeOfData": "event",
+        "pages": LIST
+      };
 
-      fetch('/crawl', {
-        method: 'post',
+      let jsonObj = JSON.stringify(pages);
+
+      fetch('/api/crawl',{
+        method:'post',
+        mode:'cors',
         headers: {
           'content-type':'application/json'
         },
-        body: JSON.stringify(pages)
+        body: jsonObj
       })
           .then(response => response.json())
-          .then(json => console.log(json));
+          .then(json => console.log(json))
+          .catch(e => console.log(e));
     };
-  });
+
+    const getData = async() => {
+      fetch('api/getData')
+          .then(response => response.json())
+          .then(json => setData(json))
+          .catch(e => console.log(e));
+    };
+
+    postData();
+    getData();
+
+  },[]);
+
+  const handleChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const handleClick = () => {
+
+  };
+
   return (
     <div className="App">
-
+      <p>You can request event data by start date, end date, location, name, or website.</p>
+      <p>Enter a search by location by entering "Las Vegas".</p>
+      <p>Enter a search by start date by entering "2022-03-30".</p>
+      <p>You will be shown all events that match you query.</p>
+      <p>Alternatively you can see all data sorted either ascending or descending order.</p>
+      <p> type "asc" for ascending and "desc" for descening.</p>
+      <input type="text" className="input" onChange={e => handleChange}/>Filter/Sort Data
+      <button onClick={}>Search</button>
     </div>
   );
 }
--- src/main/react-app/constants/pageConstants.js
+++ src/main/react-app/constants/pageConstants.js
@@ -1,12 +1,12 @@
 const PAGE1 = {
-    url : "https://www.computerworld.com/article/3313417/" +
+    "url" : "https://www.computerworld.com/article/3313417/" +
         "tech-event-calendar-2020-upcoming-shows-conferences-and-it-expos.html",
-    containerId: "cwsearchabletable"
+    "containerId": "cwsearchabletable"
 };
 
-const PAGE2= {
-    url: "https://www.techmeme.com/events",
-    containerId: "events"
+const PAGE2 = {
+    "url": "https://www.techmeme.com/events",
+    "containerId": "events"
 };
 
 export const LIST = [PAGE1, PAGE2];
